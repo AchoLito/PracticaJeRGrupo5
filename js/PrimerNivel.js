35e28d6b48f00aed941124c84985a671c7f7c1f7
UnaiPas04 <unaipastranatorres@gmail.com>
2024-11-05 10:12:18 +0100

    movimiento fantasma

--- js/PrimerNivel.js
+++ js/PrimerNivel.js
@@ -12,10 +12,15 @@ class PantallaInicio extends Phaser.Scene
 
     create()
     {
-        //humano
-        this.humano = this.physics.add.sprite(400, 530, 'NOMBRE SPRITEEEEE');
-        this.terrain.body.setImmovable(true);
-        this.terrain.body.setAllowGravity(false);
+        //HUMANO
+        this.humano = this.physics.add.sprite(40, 40, 'NOMBRE SPRITEEEEE');
+        this.humano.body.setImmovable(true);
+        this.humano.body.setAllowGravity(false);
+
+        //FANTASMA
+        this.fantasma = this.physics.add.sprite(40, 40, 'NOMBRE SPRITEEEEE');
+        this.fantasma.body.setImmovable(true);
+        this.fantasma.body.setAllowGravity(false);
 
         this.inicializarControlesHumano();
         this.inicializarControlesFantasma();
@@ -40,6 +45,23 @@ class PantallaInicio extends Phaser.Scene
 
         this.input.keyboard.on('keydown-D', humanoInput('RIGHT'));
         this.input.keyboard.on('keyup-D', humanoInput('X_NONE'));
+
+        //TECLAS ESPECIALES
+    }
+    inicializarControlesFantasma() {
+        
+        //MOVIMIENTO
+        this.input.keyboard.on('keydown-UP', fantasmaInput('UP'));
+        this.input.keyboard.on('keyup-UP', fantasmaInput('Y_NONE'));
+
+        this.input.keyboard.on('keydown-DOWN', fantasmaInput('DOWN'));
+        this.input.keyboard.on('keyup-DOWN', fantasmaInput('Y_NONE'));
+        
+        this.input.keyboard.on('keydown-LEFT', fantasmaInput('LEFT'));
+        this.input.keyboard.on('keyup-LEFT', fantasmaInput('X_NONE'));
+
+        this.input.keyboard.on('keydown-RIGHT', fantasmaInput('RIGHT'));
+        this.input.keyboard.on('keyup-RIGHT', fantasmaInput('X_NONE'));
 
         //TECLAS ESPECIALES
     }
@@ -75,23 +97,7 @@ class PantallaInicio extends Phaser.Scene
         }
     }
 
-    inicializarControlesFantasma() {
-        
-        //MOVIMIENTO
-        this.input.keyboard.on('keydown-W', humanoInput('UP'));
-        this.input.keyboard.on('keyup-W', humanoInput('Y_NONE'));
-
-        this.input.keyboard.on('keydown-S', humanoInput('DOWN'));
-        this.input.keyboard.on('keyup-S', humanoInput('Y_NONE'));
-        
-        this.input.keyboard.on('keydown-A', humanoInput('LEFT'));
-        this.input.keyboard.on('keyup-A', humanoInput('X_NONE'));
-
-        this.input.keyboard.on('keydown-D', humanoInput('RIGHT'));
-        this.input.keyboard.on('keyup-D', humanoInput('X_NONE'));
-
-        //TECLAS ESPECIALES
-    }
+    
 
     fantasmaInput(control){
         var velocidad=3;
