3ff82c4851292b20c6d1b447e8de68321e0be1a6
MagaBlanca46 <115171614+MagaBlanca46@users.noreply.github.com>
2024-11-24 17:53:07 +0100

    Actualización del script MenuInicio
    Se han añadido los sprites de los botones y se han colocado donde se requería.

--- js/MenuInicio.js
+++ js/MenuInicio.js
@@ -8,10 +8,10 @@ class MenuInicio extends Phaser.Scene
     preload()
     {
         // Hay que añadir la ruta a los archivos 
-        this.load.audio("clic"); // sonido pulsar botón
-        this.load.audio("musicaMenu"); // sonido música fondo menú
+        //this.load.audio("clic"); // sonido pulsar botón
+        //this.load.audio("musicaMenu"); // sonido música fondo menú
 
-        this.load.image("tituloJuego");
+        //this.load.image("tituloJuego");
         this.load.image("botonJugar", "imagenes/BotonJugar.png"); // imagen del botón de jugar
         this.load.image("botonControles", "imagenes/BotonControles.png"); // imagen del botón de controles
         this.load.image("botonCreditos", "imagenes/BotonCreditos.png"); // imagen del botón de créditos
@@ -19,34 +19,34 @@ class MenuInicio extends Phaser.Scene
 
     create()
     {
-        this.musica = this.sound.add("musicaMenu");
+        /*this.musica = this.sound.add("musicaMenu");
         this.musica.loop = true;
         this.musica.play();
 
-        const tituloJuego = this.add.image("tituloJuego");
+        const tituloJuego = this.add.image("tituloJuego");*/
 
-        const botonJugar = this.add.image("botonJugar") // añadir en el paréntesis la posición en la que queremos la imagen
+        const botonJugar = this.add.image(640, 320, "botonJugar") // añadir en el paréntesis la posición en la que queremos la imagen
             .setInteractive()
             .on("pointerdown", () => {
-                this.sound.play("clic");
+                //this.sound.play("clic");
                 this.scene.stop("MenuInicio");
-                this.scene.start("Historia");
+                this.scene.start("PrimerNivel");
             });
 
-        const botonControles = this.add.image("botonControles")
+        const botonControles = this.add.image(640, 470, "botonControles")
             .setInteractive()
             .on("pointerdown", () => {
-                this.sound.play("clic");
+                //this.sound.play("clic");
                 this.scene.stop("MenuInicio");
                 this.scene.start("Controles");
             });
             
-        const botonCreditos = this.add.image("botonCreditos")
+        const botonCreditos = this.add.image(640, 620, "botonCreditos")
             .setInteractive()
             .on("pointerdown", () => {
-                this.sound.play("clic");
+                //this.sound.play("clic");
                 this.scene.stop("MenuInicio");
-                this.scene.start("MenuAjustes");
+                this.scene.start("Creditos");
             });
     }
 
