424d6107bd0185493adf486addc1e89450539ff3
MagaBlanca46 <115171614+MagaBlanca46@users.noreply.github.com>
2024-11-24 20:28:45 +0100

    Actualización de la escena Controles
    Se han añadido imágenes y se han colocado, faltarían todavía imágenes y texto indicando para qué sirve cada botón.

--- js/Controles.js
+++ js/Controles.js
@@ -7,44 +7,47 @@ class Controles extends Phaser.Scene
 
     preload()
     {
-        this.load.audio("clic"); // sonido pulsar botón
-        this.load.audio("musicaMenu"); // sonido música fondo menú
+        /*this.load.audio("clic"); // sonido pulsar botón
+        this.load.audio("musicaMenu"); // sonido música fondo menú*/
 
-        this.load.image("humano"); // imagen humano
-        this.load.image("fantasma"); // imagen fantasma
-        this.load.image("botonVolver"); // imagen del botón volver
-        this.load.image("WASD"); // movimiento humano
-        this.load.image("flechas"); // movimiento fantasma
-        this.load.image("TeclaE"); // interacción humano
-        this.load.image("Intro"); // interacción fantasma 
-
-        this.load.text("Controles"); // añadir delante la posición y detrás el tipo de fuente
-        this.load.text("Humano"); 
-        this.load.text("Fantasma");
+        //this.load.image("humano"); // imagen humano
+        this.load.image("fantasma", "imagenes/FANTASMA_DELANTE.png"); // imagen fantasma
+        this.load.image("botonVolver", "imagenes/BotonVolver.png"); // imagen del botón volver
+        this.load.image("WASD", "imagenes/TeclasWASD.png"); // movimiento humano
+        this.load.image("flechas", "imagenes/TeclasFlechas.png"); // movimiento fantasma
+        this.load.image("TeclaE", "imagenes/TeclaE.png"); // interacción humano
+        this.load.image("Intro", "imagenes/TeclaIntro.png"); // interacción fantasma 
+        this.load.image("espacio", "imagenes/TeclaEspacio.png"); // tecla para pasar los diálogos
     }
 
     create()
     {
-        this.musica = this.sound.add("musicaMenu");
+        /*this.musica = this.sound.add("musicaMenu");
         this.musica.loop = true;
         this.musica.play();
 
-        const humano = this.add.image("humano");
+        const humano = this.add.image("humano");*/
 
-        const fantasma = this.add.image("fantasma");
+        //const textHumano = this.add.text("Humano");
 
-        const wasd = this.add.image("WASD");
+        const fantasma = this.add.image(300, 700, "fantasma");
 
-        const flechas = this.add.image("flechas");
+        //const textFantasma = this.add.text("Fantasma");
 
-        const teclaE = this.add.image("TeclaE");
+        const wasd = this.add.image(700, 200, "WASD");
 
-        const intro = this.add.image("Intro");
+        const teclaE = this.add.image(1100, 200, "TeclaE");
 
-        const botonVolver = this.add.image("botonVolver")
+        const flechas = this.add.image(700, 700, "flechas");
+
+        const intro = this.add.image(1100, 700, "Intro");
+
+        const espacio = this.add.image(925, 450, "espacio");
+
+        const botonVolver = this.add.image(75, 75, "botonVolver")
             .setInteractive()
             .on("pointerdown", () => {
-                this.sound.play("clic");
+                //this.sound.play("clic");
                 this.scene.stop("Controles");
                 this.scene.start("MenuInicio");
             });
