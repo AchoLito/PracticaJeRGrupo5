6f96ff457c156dbabac904e48b854ace3b735f58
MagaBlanca46 <115171614+MagaBlanca46@users.noreply.github.com>
2024-11-24 17:52:10 +0100

    Actualización del scripts
    Se ha añadido el sprite del botón de continuar y se ha colocado en la posición deseada.

--- js/PantallaInicio.js
+++ js/PantallaInicio.js
@@ -8,22 +8,28 @@ class PantallaInicio extends Phaser.Scene
     preload()
     {
         // Hay que añadir la ruta a los archivos
-        this.load.audio("clic"); // sonido pulsar botón
-        this.load.audio("musicaInicio"); // sonido música fondo 
+        //this.load.audio("clic"); // sonido pulsar botón
+        //this.load.audio("musicaInicio"); // sonido música fondo 
 
-        this.load.image("botonContinuar"); // imagen del botón de continuar
+        //this.load.image("fondo"); // imagen del fondo
+        //this.load.image("tituloJuego"); // imagen con las letras del título
+        this.load.image("botonContinuar", "imagenes/BotonContinuar.png"); // imagen del botón de continuar
     }
 
     create()
     {
-        this.musica = this.sound.add("musicaInicio");
-        this.musica.loop = true;
-        this.musica.play();
+        //this.musica = this.sound.add("musicaInicio");
+        //this.musica.loop = true;
+        //this.musica.play();
 
-        const botonJugar = this.add.image("botonContinuar") // añadir en el paréntesis la posición en la que queremos la imagen
+        //const fondo = this.add.image("fondo");
+
+        //const titulo = this.add.image("tituloJuego");
+
+        const botonJugar = this.add.image(640, 800, "botonContinuar") // añadir en el paréntesis la posición en la que queremos la imagen
             .setInteractive()
             .on("pointerdown", () => {
-                this.sound.play("clic");
+                //this.sound.play("clic");
                 this.scene.stop("PantallaInicio");
                 this.scene.start("MenuInicio");
             });
