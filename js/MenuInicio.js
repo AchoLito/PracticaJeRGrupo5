2373acead31e7a84ddd12c9978b224cdf4712a93
MagaBlanca46 <115171614+MagaBlanca46@users.noreply.github.com>
2024-11-26 21:00:16 +0100

    Escena menú inicio final
    Escena que contiene el menú de inicio terminada, con los correspondientes sprites y el ruido de los botones.

--- js/MenuInicio.js
+++ js/MenuInicio.js
@@ -8,10 +8,8 @@ class MenuInicio extends Phaser.Scene
     preload()
     {
         // Hay que añadir la ruta a los archivos 
-        //this.load.audio("clic"); // sonido pulsar botón
-        //this.load.audio("musicaMenu"); // sonido música fondo menú
-
-        //this.load.image("tituloJuego");
+        this.load.audio("clic", "audio/clic.mp3"); // sonido pulsar botón
+      
         this.load.image("Fondo_menu", "imagenes/FONDO_MENÚ_INICIO.png"); 
         this.load.image("botonJugar", "imagenes/BotonJugar.png"); // imagen del botón de jugar
         this.load.image("botonControles", "imagenes/BotonControles.png"); // imagen del botón de controles
@@ -20,18 +18,12 @@ class MenuInicio extends Phaser.Scene
 
     create()
     {
-        /*this.musica = this.sound.add("musicaMenu");
-        this.musica.loop = true;
-        this.musica.play();
-
-        const tituloJuego = this.add.image("tituloJuego");*/
-
         this.add.image(1280/2,900/2,'Fondo_menu');
 
         const botonJugar = this.add.image(640, 320, "botonJugar") // añadir en el paréntesis la posición en la que queremos la imagen
             .setInteractive()
             .on("pointerdown", () => {
-                //this.sound.play("clic");
+                this.sound.play("clic");
                 this.scene.stop("MenuInicio");
                 this.scene.start("PrimerNivel");
             });
@@ -39,7 +31,7 @@ class MenuInicio extends Phaser.Scene
         const botonControles = this.add.image(640, 470, "botonControles")
             .setInteractive()
             .on("pointerdown", () => {
-                //this.sound.play("clic");
+                this.sound.play("clic");
                 this.scene.stop("MenuInicio");
                 this.scene.start("Controles");
             });
@@ -47,7 +39,7 @@ class MenuInicio extends Phaser.Scene
         const botonCreditos = this.add.image(640, 620, "botonCreditos")
             .setInteractive()
             .on("pointerdown", () => {
-                //this.sound.play("clic");
+                this.sound.play("clic");
                 this.scene.stop("MenuInicio");
                 this.scene.start("Creditos");
             });
