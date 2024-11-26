6e5d6e71b95022d17b0cec4622504417a8de5b65
MagaBlanca46 <115171614+MagaBlanca46@users.noreply.github.com>
2024-11-26 19:19:07 +0100

    Actualización de la escena PantallaInicio
    Todos los sprites y la interfaz están colocados, faltan la música y los efectos de sonido del botón.

--- js/PantallaInicio.js
+++ js/PantallaInicio.js
@@ -11,8 +11,8 @@ class PantallaInicio extends Phaser.Scene
         //this.load.audio("clic"); // sonido pulsar botón
         //this.load.audio("musicaInicio"); // sonido música fondo 
 
-        //this.load.image("fondo"); // imagen del fondo
-        //this.load.image("tituloJuego"); // imagen con las letras del título
+        this.load.image("fondo", "imagenes/FONDO_MENÚ_INICIO.png"); // imagen del fondo
+        this.load.image("tituloJuego", "imagenes/TÍTULO.png"); // imagen con las letras del título
         this.load.image("botonContinuar", "imagenes/BotonContinuar.png"); // imagen del botón de continuar
     }
 
@@ -22,9 +22,9 @@ class PantallaInicio extends Phaser.Scene
         //this.musica.loop = true;
         //this.musica.play();
 
-        //const fondo = this.add.image("fondo");
+        const fondo = this.add.image(640, 450, "fondo");
 
-        //const titulo = this.add.image("tituloJuego");
+        const titulo = this.add.image(500, 150, "tituloJuego");
 
         const botonJugar = this.add.image(640, 800, "botonContinuar") // añadir en el paréntesis la posición en la que queremos la imagen
             .setInteractive()
