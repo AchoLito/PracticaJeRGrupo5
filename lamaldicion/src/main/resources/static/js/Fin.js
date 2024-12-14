3c0f29b6421ff642c408b3536f7c8cce670f575d
MagaBlanca46 <anapro46@gmail.com>
2024-12-14 17:05:09 +0100

    Botón pantalla final
    Se ha añadido un botón en la pantalla del final para poder volver al menú inicial

--- lamaldicion/src/main/resources/static/js/Fin.js
+++ lamaldicion/src/main/resources/static/js/Fin.js
@@ -7,12 +7,20 @@ class Fin extends Phaser.Scene
 
     preload()
     {
-        
+        this.load.image("botonRegresar", "imagenes/botonRegresar.png");
     }
 
     create()
     {
         this.add.text(270, 370, "Continuará", { fontFamily: "sans-serif", fontSize: "150px", color: "#FFFFFF" });
+    
+        const botonVolver = this.add.image(640, 800, "botonRegresar")
+            .setInteractive()
+            .on("pointerdown", () => {
+                this.sound.play("clic");
+                this.scene.stop("Fin");
+                this.scene.start("MenuInicio");
+            });
     }
 
     update()
