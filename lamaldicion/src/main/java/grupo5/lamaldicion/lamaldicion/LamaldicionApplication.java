1822122e07f05642c5ddd830ac4e30e37871ac13
AchoLito <javier.martinmulero@gmail.com>
2024-12-14 20:37:38 +0100

    UsersController login
    Se ha implementado la parte de java del login de usuarios

--- lamaldicion/src/main/java/grupo5/lamaldicion/lamaldicion/LamaldicionApplication.java
+++ lamaldicion/src/main/java/grupo5/lamaldicion/lamaldicion/LamaldicionApplication.java
@@ -2,8 +2,12 @@ package grupo5.lamaldicion.lamaldicion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"java.lang.String"})
+@EntityScan("java.lang.String")
 public class LamaldicionApplication {
 
 	public static void main(String[] args) {
---
+++ lamaldicion/src/main/java/grupo5/lamaldicion/lamaldicion/User.java
@@ -0,0 +1,40 @@
+package grupo5.lamaldicion.lamaldicion;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+public class User {
+    private String name;
+    private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public void setName(String n)
+    {
+        this.name = n;
+    }
+    public void setPassword(String p)
+    {
+        this.password = p;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getPassword()
+    {
+        return this.password;
+    }
+}
