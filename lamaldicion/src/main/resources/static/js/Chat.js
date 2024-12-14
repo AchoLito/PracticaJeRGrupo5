7d9d782c6a742f197e820c04358d37082c57a2d4
UnaiPas04 <unaipastranatorres@gmail.com>
2024-12-14 18:10:44 +0100

    clase chat

---
+++ lamaldicion/src/main/resources/static/js/Chat.js
@@ -0,0 +1,24 @@
+class Chat extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: "Chat"});
+    }
+
+    preload()
+    {
+        this.load.image('CHAT_Fondo', 'imagenes/chatFondo.png');
+    }
+
+    create()
+    {
+        
+
+    }
+    
+    update()
+    {
+
+    }
+    
+}
