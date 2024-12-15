81e935974fd154656e54bdee4b33dc3a031aaa50
AchoLito <javier.martinmulero@gmail.com>
2024-12-15 01:35:14 +0100

    Login js
    Se ha acabado de implementar la parte de JS del login

--- lamaldicion/src/main/resources/static/js/LogIn.js
+++ lamaldicion/src/main/resources/static/js/LogIn.js
@@ -17,23 +17,26 @@ class LogIn extends Phaser.Scene
         const users = this.cache.json.get("acho",);
         const formulario = this.add.dom(590, 400).createFromCache('logIn');
         const nombreInputs = formulario.getChildByID("nombreUsuario");
+        const scene = this.scene;
         console.log(formulario);
-        console.log(users.key);
+        console.log(users[0].name);
+        console.log(nombreInputs.value);
         formulario.addListener("click");
 
         formulario.on("click", function (event)
         {
             if (event.target.name === "botonLogIn")
             {
-                const nombreInput = formulario.getChildByID("nombreUsuario");
-                const passwordInput = formulario.getChildByID("password");
+                const nombreInput = this.getChildByID("nombreUsuario");
+                const passwordInput = this.getChildByID("password");               
 
                 if (nombreInput.value !== "" && passwordInput.value !== "")
                 {
                     //if (nombreInput.value === users.name && passwordInput.value === users.password)
-                    if (users.find("Acho") && passwordInput.value === users.password)
+                    console.log(this.scene.compruebaNombre(users, nombreInput.value) && this.scene.compruebaPassword(users, passwordInput.value));
+                    if (this.scene.compruebaNombre(users, nombreInput.value) && this.scene.compruebaPassword(users, passwordInput.value))
                     {
-                        formulario.removeListener("click");
+                        this.removeListener("click");
                         console.log("hola");
                         this.scene.tweens.add({
                             targets: formulario, scaleX: 2, scaleY: 2, y: 700, duration: 3000, ease: 'Power3',
@@ -43,9 +46,9 @@ class LogIn extends Phaser.Scene
                             }
                         });
                         
-                        this.scene.stop("LogIn");
-                        this.scene.start("PantallaInicio");
-                    }
+                        scene.stop("LogIn");
+                        scene.start("PantallaInicio");
+                    }              
                 }
             }
 
@@ -56,6 +59,34 @@ class LogIn extends Phaser.Scene
             y: 300,
             duration: 3000,
             ease: 'Power3'
-        });
+        });       
+    }    
+
+    compruebaNombre(array, nombre)
+    {
+        for(var i = 0; i < array.length; i++)
+        {
+            console.log(nombre + " " + array[i].name);
+            if(nombre == array[i].name)
+            {
+                return true;
+            }
+        }
+
+        return false
+    }
+
+    compruebaPassword(array, password)
+    {
+        for(var i = 0; i < array.length; i++)
+        {
+            console.log(password + " " + array[i].password);
+            if(password == array[i].password)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
