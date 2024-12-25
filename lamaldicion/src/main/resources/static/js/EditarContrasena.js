9474ee365ccfd25994f082d1ff8788855ed4e489
MagaBlanca46 <anapro46@gmail.com>
2024-12-25 18:55:27 +0100

    Nueva escena
    Escena a la que se va a cambiar cuando se quiera cambiar la contraseña del perfil

---
+++ lamaldicion/src/main/resources/static/js/EditarContrasena.js
@@ -0,0 +1,22 @@
+class EditarContrasena extends Phaser.Scene
+{
+    constructor()
+    {
+        super({ key: "EditarContrasena"}); 
+    }
+
+    preload()
+    {
+
+    }
+
+    create()
+    {
+
+    }
+
+    update()
+    {
+        
+    }
+}
